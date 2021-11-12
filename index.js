bf808ec586426d40ce560336f3fe4b1db42ef4ea
shaibya1986 <60124795+shaibya1986@users.noreply.github.com>
2021-11-12 11:46:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let numbersArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
+cubeWrapper = document.getElementById('cube-container'), appendCubeElement;
+
+numbersArray.forEach((item) => {
+  cubeWrapper.innerHTML += '<li data-item=' + item + '>' + item + '</li>'
+})
+shuffleArray = () => {
+  let index = 0
+  numbersArray = numbersArray.sort(() => { return .5 - Math.random() });
+  cubeWrapper.innerHTML = ''
+  while (index < numbersArray.length) {
+    cubeWrapper.innerHTML += '<li data-item=' + numbersArray[index] + '>' + numbersArray[index] + '</li>'
+    ++index;
+  }
+}
+sortArray = () => {
+  let index = 0
+  numbersArray = numbersArray.sort((a, b) => { return a - b });
+  cubeWrapper.innerHTML = ''
+  while (index < numbersArray.length) {
+    cubeWrapper.innerHTML += '<li data-item=' + numbersArray[index] + '>' + numbersArray[index] + '</li>'
+    ++index;
+  }
+} 
